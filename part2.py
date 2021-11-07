c1d89db9ae37fbc8600ba3e87648d729677cdd31
AlexandraBern <79848052+AlexandraBernstein@users.noreply.github.com>
2021-11-07 16:12:06 +0000

    I wrote the codes for parts 1 and 2.

--- part2.py
+++ part2.py
@@ -31,4 +31,21 @@ diamond(5) -->
 """
 
 def diamond(size):
-  pass  # delete this when you start writing your code
+  for i in range(1, size + 1):
+    for j in range(1, size - i + 1):
+      print(end = ' ')
+    for k in range(1, 2 * i):
+      if k == 1 or k == i * 2 - 1:
+        print('*', end = '')
+      else:
+        print(' ', end = '')
+    print()
+  for i in range(size - 1, 0, -1):
+    for j in range(1, size - i + 1):
+      print(' ', end = '')
+    for k in range(1, 2 * i):
+      if k == 1 or k == i * 2 - 1:
+        print('*', end = '')
+      else:
+        print(' ', end = '')
+    print()
